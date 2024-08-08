0e974315d8f34d301924e09ea362ebf48d4dc33a
aimll <1828554753@qq.com>
2024-08-08 12:44:35 +0800

    不完全统一单个视频的图像的退化

--- realesrgan_degration.py
+++ realesrgan_degration.py
@@ -21,9 +21,8 @@ import PIL.Image as Image
 
 
 class RealESRGANDatadegration():
-    def __init__(self, img_paths):
+    def __init__(self):
         super(RealESRGANDatadegration, self).__init__()
-        self.paths = img_paths
         self.scale = 1  # 缩放比例
         # self.gt_size = gt_size #输入图像的尺寸，需要h=w
         gpu_id = None  # 0
@@ -35,23 +34,23 @@ class RealESRGANDatadegration():
             self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
 
         # the first degradation process
-        self.resize_prob = [0.2, 0.7, 0.1]  # up, down, keep
+        self.resize_prob = [0.1, 0.9, 0.0]  # up, down, keep
         self.resize_range = [0.15, 1.5]
-        self.gaussian_noise_prob = 0.5
+        self.gaussian_noise_prob = 0.8
         self.noise_range = [1, 30]
         self.poisson_scale_range = [0.05, 3]
-        self.gray_noise_prob = 0.4
-        self.jpeg_range = [30, 95]
+        self.gray_noise_prob = 1
+        self.jpeg_range = [20, 30]
 
         # the second degradation process
         self.second_blur_prob = 0.8
-        self.resize_prob2 = [0.3, 0.4, 0.3]  # up, down, keep
+        self.resize_prob2 = [0.1, 0.9, 0.0]  # up, down, keep
         self.resize_range2 = [0.3, 1.2]
-        self.gaussian_noise_prob2 = 0.5
+        self.gaussian_noise_prob2 = 0.8
         self.noise_range2 = [1, 25]
         self.poisson_scale_range2 = [0.05, 2.5]
-        self.gray_noise_prob2 = 0.4
-        self.jpeg_range2 = [30, 95]
+        self.gray_noise_prob2 = 1
+        self.jpeg_range2 = [20, 30]
 
         # blur settings for the first degradation
         self.blur_kernel_size = 21
@@ -85,6 +84,43 @@ class RealESRGANDatadegration():
         self.file_client = None
         self.usm_sharpener = USMSharp().to(self.device)  # do usm sharpening
         self.jpeger = DiffJPEG(differentiable=False).to(self.device)  # # simulate JPEG compression artifacts
+        self.kernel_size = random.choice(self.kernel_range)
+        self.kernel_1 = random_mixed_kernels(
+            self.kernel_list,
+            self.kernel_prob,
+            self.kernel_size,
+            self.blur_sigma,
+            self.blur_sigma, [-math.pi, math.pi],
+            self.betag_range,
+            self.betap_range,
+            noise_range=None)
+        self.kernel_size_2 = random.choice(self.kernel_range)
+        self.kernel_size_3 = random.choice(self.kernel_range)
+        # random resize
+        self.updown_type_1 = random.choices(['up', 'down', 'keep'], self.resize_prob)[0]
+        if self.updown_type_1 == 'up':
+            self.scale_1 = np.random.uniform(1, self.resize_range[1])
+        elif self.updown_type_1 == 'down':
+            self.scale_1 = np.random.uniform(self.resize_range[0], 1)
+        else:
+            self.scale_1 = 1
+        self.mode_1 = random.choice(['area', 'bilinear', 'bicubic'])
+        self.gaussian_1 = np.random.uniform()
+        self.blur_1 = np.random.uniform()
+        # random resize
+        self.updown_type_2 = random.choices(['up', 'down', 'keep'], self.resize_prob2)[0]
+        if self.updown_type_2 == 'up':
+            self.scale_2 = np.random.uniform(1, self.resize_range2[1])
+        elif self.updown_type_2 == 'down':
+            self.scale_2 = np.random.uniform(self.resize_range2[0], 1)
+        else:
+            self.scale_2 = 1
+        self.mode_2 = random.choice(['area', 'bilinear', 'bicubic'])
+        self.gaussian_2 = np.random.uniform()
+
+        self.random_number = np.random.uniform()
+
+        self.mode_3 = random.choice(['area', 'bilinear', 'bicubic'])
 
     @torch.no_grad()
     def kernel(self):
@@ -134,41 +170,31 @@ class RealESRGANDatadegration():
         #     img_gt = img_gt[top:top + crop_pad_size, left:left + crop_pad_size, ...]
 
         # ------------------------ Generate kernels (used in the first degradation) ------------------------ #
-        kernel_size = random.choice(self.kernel_range)
         if np.random.uniform() < self.sinc_prob:
             # this sinc filter setting is for kernels ranging from [7, 21]
-            if kernel_size < 13:
+            if self.kernel_size < 13:
                 omega_c = np.random.uniform(np.pi / 3, np.pi)
             else:
                 omega_c = np.random.uniform(np.pi / 5, np.pi)
-            kernel = circular_lowpass_kernel(omega_c, kernel_size, pad_to=False)
+            kernel = circular_lowpass_kernel(omega_c, self.kernel_size, pad_to=False)
         else:
-            kernel = random_mixed_kernels(
-                self.kernel_list,
-                self.kernel_prob,
-                kernel_size,
-                self.blur_sigma,
-                self.blur_sigma, [-math.pi, math.pi],
-                self.betag_range,
-                self.betap_range,
-                noise_range=None)
+            kernel = self.kernel_1
         # pad kernel
-        pad_size = (21 - kernel_size) // 2
+        pad_size = (21 - self.kernel_size) // 2
         kernel = np.pad(kernel, ((pad_size, pad_size), (pad_size, pad_size)))
 
         # ------------------------ Generate kernels (used in the second degradation) ------------------------ #
-        kernel_size = random.choice(self.kernel_range)
         if np.random.uniform() < self.sinc_prob2:
-            if kernel_size < 13:
+            if self.kernel_size_2 < 13:
                 omega_c = np.random.uniform(np.pi / 3, np.pi)
             else:
                 omega_c = np.random.uniform(np.pi / 5, np.pi)
-            kernel2 = circular_lowpass_kernel(omega_c, kernel_size, pad_to=False)
+            kernel2 = circular_lowpass_kernel(omega_c, self.kernel_size_2, pad_to=False)
         else:
             kernel2 = random_mixed_kernels(
                 self.kernel_list2,
                 self.kernel_prob2,
-                kernel_size,
+                self.kernel_size_2,
                 self.blur_sigma2,
                 self.blur_sigma2, [-math.pi, math.pi],
                 self.betag_range2,
@@ -176,14 +202,13 @@ class RealESRGANDatadegration():
                 noise_range=None)
 
         # pad kernel
-        pad_size = (21 - kernel_size) // 2
+        pad_size = (21 - self.kernel_size_2) // 2
         kernel2 = np.pad(kernel2, ((pad_size, pad_size), (pad_size, pad_size)))
 
         # ------------------------------------- the final sinc kernel ------------------------------------- #
         if np.random.uniform() < self.final_sinc_prob:
-            kernel_size = random.choice(self.kernel_range)
             omega_c = np.random.uniform(np.pi / 3, np.pi)
-            sinc_kernel = circular_lowpass_kernel(omega_c, kernel_size, pad_to=21)
+            sinc_kernel = circular_lowpass_kernel(omega_c, self.kernel_size_3, pad_to=21)
             sinc_kernel = torch.FloatTensor(sinc_kernel)
         else:
             sinc_kernel = self.pulse_tensor
@@ -199,10 +224,11 @@ class RealESRGANDatadegration():
         return return_d
 
     @torch.no_grad()
-    def synthesis(self):
+    def synthesis(self, img_paths):
         """Accept data from dataloader, and then add two-order degradations to obtain LQ images.
         """
         # training data synthesis
+        self.paths = img_paths
         data = self.kernel()
         gt = data['gt'].to(self.device)
         self.gt = gt.unsqueeze(0)
@@ -216,19 +242,11 @@ class RealESRGANDatadegration():
         # ----------------------- The first degradation process ----------------------- #
         # blur
         out = filter2D(self.gt_usm, self.kernel1)
-        # random resize
-        updown_type = random.choices(['up', 'down', 'keep'], self.resize_prob)[0]
-        if updown_type == 'up':
-            scale = np.random.uniform(1, self.resize_range[1])
-        elif updown_type == 'down':
-            scale = np.random.uniform(self.resize_range[0], 1)
-        else:
-            scale = 1
-        mode = random.choice(['area', 'bilinear', 'bicubic'])
-        out = F.interpolate(out, scale_factor=scale, mode=mode)
+
+        out = F.interpolate(out, scale_factor=self.scale_1, mode=self.mode_1)
         # add noise
         gray_noise_prob = self.gray_noise_prob
-        if np.random.uniform() < self.gaussian_noise_prob:
+        if self.gaussian_1 < self.gaussian_noise_prob:
             out = random_add_gaussian_noise_pt(
                 out, sigma_range=self.noise_range, clip=True, rounds=False, gray_prob=gray_noise_prob)
         else:
@@ -245,23 +263,15 @@ class RealESRGANDatadegration():
 
         # ----------------------- The second degradation process ----------------------- #
         # blur
-        if np.random.uniform() < self.second_blur_prob:
+        if self.blur_1 < self.second_blur_prob:
             out = filter2D(out, self.kernel2)
-        # random resize
-        updown_type = random.choices(['up', 'down', 'keep'], self.resize_prob2)[0]
-        if updown_type == 'up':
-            scale = np.random.uniform(1, self.resize_range2[1])
-        elif updown_type == 'down':
-            scale = np.random.uniform(self.resize_range2[0], 1)
-        else:
-            scale = 1
-        mode = random.choice(['area', 'bilinear', 'bicubic'])
+
         out = F.interpolate(
-            out, size=(int(ori_h / self.scale * scale), int(ori_w / self.scale * scale)),
-            mode=mode)
+            out, size=(int(ori_h / self.scale * self.scale_2), int(ori_w / self.scale * self.scale_2)),
+            mode=self.mode_2)
         # add noise
         gray_noise_prob = self.gray_noise_prob2
-        if np.random.uniform() < self.gaussian_noise_prob2:
+        if self.gaussian_2 < self.gaussian_noise_prob2:
             out = random_add_gaussian_noise_pt(
                 out, sigma_range=self.noise_range2, clip=True, rounds=False, gray_prob=gray_noise_prob)
         else:
@@ -279,10 +289,9 @@ class RealESRGANDatadegration():
         #   1. [resize back + sinc filter] + JPEG compression
         #   2. JPEG compression + [resize back + sinc filter]
         # Empirically, we find other combinations (sinc + JPEG + Resize) will introduce twisted lines.
-        if np.random.uniform() < 0.5:
+        if self.random_number < 0.5:
             # resize back + the final sinc filter
-            mode = random.choice(['area', 'bilinear', 'bicubic'])
-            out = F.interpolate(out, size=(ori_h // self.scale, ori_w // self.scale), mode=mode)
+            out = F.interpolate(out, size=(ori_h // self.scale, ori_w // self.scale), mode=self.mode_3)
             out = filter2D(out, self.sinc_kernel)
             # JPEG compression
             jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.jpeg_range2)
@@ -319,30 +328,40 @@ class RealESRGANDatadegration():
 
 
 if __name__ == '__main__':
-    def get_image_list(folder_path):
-        image_list = []
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.png'):
-                    image_path = os.path.join(root, file)
-                    image_list.append(image_path)
-        return image_list
+
+    def collect_image_paths(root_dir, extensions=None):
+        if extensions is None:
+            extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')  # 将列表转换为元组
+        image_paths = []  # 用于存储所有图片路径的列表
+        folders_images = []  # 二维列表，存储每个文件夹的图片路径列表
+
+        # 遍历根目录及其所有子目录
+        for root, dirs, files in os.walk(root_dir):
+            # 过滤出当前目录中的图片文件
+            # 使用tuple(extensions)确保endswith()方法接收到的是字符串或字符串元组
+            images = [os.path.join(root, file) for file in files if file.lower().endswith(tuple(extensions))]
+            # 如果当前目录中有图片，将其路径列表添加到image_paths中
+            if images:
+                image_paths.extend(images)
+                # 将图片路径列表添加到二维列表中，表示当前文件夹的图片
+                folders_images.append(images)
+
+        return folders_images
 
 
-    # 使用示例：遍历多个文件夹
-    folder1_path = "/home/aimll/mot/data/Dataset/mot/DanceTrack"
+    # 指定你的根目录路径
+    root_directory = "/home/aimll/mot/data/Dataset/mot/DanceTrack"
+    # 调用函数，获取按子文件夹分组的图片路径二维列表
+    grouped_image_paths = collect_image_paths(root_directory)
 
-    image_list1 = get_image_list(folder1_path)
-    # file_path = 'output.txt'
-    # file = open(file_path, 'w')
-    # list_str = '\n'.join(map(str, image_list1))
-    # file.write(list_str)
-    # file.close()
+    for i in grouped_image_paths:
+        t = RealESRGANDatadegration()
+        print(i[0])
+        for j in i:
+            result = t.synthesis(j)
+            torchvision.utils.save_image(result, j)
 
-    for i in image_list1:
-        t = RealESRGANDatadegration(i)
-        result = t.synthesis()
-        torchvision.utils.save_image(result, i)
+
 
 
 
